fe7cd1ff607ee855d1c681a0f7890a8720416017
alexey <asivlev@yandex.ru>
2023-02-12 17:52:18 +0300

    Creation of the 1-st file

---
+++ Sem_1_Hmw_Task1.java
@@ -0,0 +1,8 @@
+/*
+Вычислить n-ое треугольного числа (сумма чисел от 1 до n), n! (произведение чисел от 1 до n).
+ */
+
+public class Sem_1_Hmw_Task1 {
+    public static void main(String[] args) {
+    }
+}
